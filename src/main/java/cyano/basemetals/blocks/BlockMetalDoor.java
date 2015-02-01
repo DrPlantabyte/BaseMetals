fe515ec74dd098667d71b2d4f43c41628c0552ec
CCHall <explosivegnome@yahoo.com>
2015-01-31 20:01:30 -0500

    THE DOORS WORK!!!

--- src/main/java/cyano/basemetals/blocks/BlockMetalDoor.java
+++ src/main/java/cyano/basemetals/blocks/BlockMetalDoor.java
@@ -1,7 +1,12 @@
 package cyano.basemetals.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockDoor;
+import net.minecraft.block.BlockDoor.EnumDoorHalf;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -18,6 +23,7 @@ public class BlockMetalDoor extends net.minecraft.block.BlockDoor{
 		this.metal = metal;
 		this.blockHardness = metal.getMetalBlockHardness();
 		this.blockResistance = metal.getBlastResistance();
+		this.disableStats();
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -25,5 +31,10 @@ public class BlockMetalDoor extends net.minecraft.block.BlockDoor{
     public Item getItem(final World w, final BlockPos c) {
         return cyano.basemetals.init.Items.getDoorItemForBlock(this);
     }
+
+	@Override
+	public Item getItemDropped(final IBlockState bs, final Random prng, final int i) {
+		return (bs.getValue(BlockDoor.HALF) == EnumDoorHalf.UPPER) ? null : cyano.basemetals.init.Items.getDoorItemForBlock(this);
+	}
 
 }
