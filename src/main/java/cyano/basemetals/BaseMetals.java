f2736fca1b54782d94438b0862be9d660f69ff13
CCHall <explosivegnome@yahoo.com>
2015-02-01 20:47:07 -0500

    Fixed another typo. All that's left to do is ore spawning

--- src/main/java/cyano/basemetals/BaseMetals.java
+++ src/main/java/cyano/basemetals/BaseMetals.java
@@ -17,7 +17,7 @@ public class BaseMetals
 
 	public static final String MODID = "basemetals";
 	public static final String NAME ="Base Metals";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.0.0";
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
