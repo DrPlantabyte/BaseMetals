e9ce364bbf5fb6ad9efd983eea5c2f1499bbaf49
CCHall <explosivegnome@yahoo.com>
2015-03-08 12:24:51 -0400

    fixed metal doors

--- src/main/java/cyano/basemetals/blocks/BlockMetalDoor.java
+++ src/main/java/cyano/basemetals/blocks/BlockMetalDoor.java
@@ -7,8 +7,10 @@ import net.minecraft.block.BlockDoor;
 import net.minecraft.block.BlockDoor.EnumDoorHalf;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -37,5 +39,24 @@ public class BlockMetalDoor extends net.minecraft.block.BlockDoor{
 	public Item getItemDropped(final IBlockState bs, final Random prng, final int i) {
 		return (bs.getValue(BlockDoor.HALF) == EnumDoorHalf.UPPER) ? null : cyano.basemetals.init.Items.getDoorItemForBlock(this);
 	}
+	
+	@Override
+    public boolean onBlockActivated(final World world, final BlockPos coord, IBlockState blockstate, 
+    		final EntityPlayer player, final EnumFacing face, 
+    		final float partialX, final float partialY, final float partialZ) {
+        if (this.blockMaterial == Material.iron) {
+            return false;
+        }
+        final BlockPos blockpos1 = (blockstate.getValue(BlockDoor.HALF) == EnumDoorHalf.LOWER) ? coord : coord.down();
+        final IBlockState iblockstate1 = coord.equals(blockpos1) ? blockstate : world.getBlockState(blockpos1);
+        if (iblockstate1.getBlock() != this) {
+            return false;
+        }
+        blockstate = iblockstate1.cycleProperty(BlockDoor.OPEN);
+        world.setBlockState(blockpos1, blockstate, 2);
+        world.markBlockRangeForRenderUpdate(blockpos1, coord);
+        world.playAuxSFXAtEntity(player, ((Boolean)blockstate.getValue(BlockDoor.OPEN)) ? 1003 : 1006, coord, 0);
+        return true;
+    }
 
 }
