fa0a693b345a553891af3ee551e87a150608caf3
Chris Hall <explosivegnome@yahoo.com>
2016-04-29 07:48:54 -0400

    improved textures of starsteel blocks and plates

--- src/main/java/cyano/basemetals/BaseMetals.java
+++ src/main/java/cyano/basemetals/BaseMetals.java
@@ -53,7 +53,7 @@ public class BaseMetals
 	public static final String NAME ="Base Metals";
 	/** Version number, in Major.Minor.Build format. The minor number is increased whenever a change 
 	 * is made that has the potential to break compatibility with other mods that depend on this one. */
-	public static final String VERSION = "2.2.1";
+	public static final String VERSION = "2.2.2";
 	
 	/** All ore-spawn files discovered in the ore-spawn folder */
 	public static final List<Path> oreSpawnConfigFiles = new LinkedList<>();
